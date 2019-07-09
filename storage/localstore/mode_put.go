b47349e569a561e0f2d6e9103fb85f924c493086
Anton Evangelatov <anton.evangelatov@gmail.com>
2019-07-09 18:43:04 +0200

    traces for chunsk in localstore and subscribepush

--- storage/localstore/mode_put.go
+++ storage/localstore/mode_put.go
@@ -23,6 +23,7 @@ import (
 
 	"github.com/ethereum/go-ethereum/metrics"
 	"github.com/ethersphere/swarm/chunk"
+	"github.com/ethersphere/swarm/log"
 	"github.com/ethersphere/swarm/shed"
 	"github.com/syndtr/goleveldb/leveldb"
 )
@@ -125,6 +126,7 @@ func (db *DB) put(mode chunk.ModePut, item shed.Item) (exists bool, err error) {
 		if !exists {
 			item.StoreTimestamp = now()
 			item.BinID, err = db.binIDs.IncInBatch(batch, uint64(db.po(item.Address)))
+			log.Debug("chunk.modeputupload", "ref", fmt.Sprintf("%x", item.Address), "binid", item.BinID, "err", err)
 			if err != nil {
 				return false, err
 			}
